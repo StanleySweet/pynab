091d2e7196e672435edd68a1c555657338ff44dd
Stanislas Dolcini <StanleySweet@users.noreply.github.com>
2026-06-02 17:36:31 +0200

    Add missing settings view for radio.

--- nabradio/views.py
+++ nabradio/views.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 from . import rfid_data
+from .models import Config
+
+
+class SettingsView(TemplateView):
+    template_name = "nabradio/settings.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["config"] = Config.load()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        config = Config.load()
+        if "streaming_url" in request.POST:
+            config.streaming_url = request.POST["streaming_url"]
+        config.save()
+        context = super().get_context_data(**kwargs)
+        context["config"] = config
+        return render(request, SettingsView.template_name, context=context)
 
 
 class RFIDDataView(TemplateView):
