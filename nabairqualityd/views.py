58ca4ae3ece19b536b078985948530652d5af4a4
Stanislas Daniel Claude Dolcini <stanislas.dolcini@gmail.com>
2026-06-04 22:22:46 +0200

    fix: same timing edge case in nabairqualityd PUT handler

--- nabairqualityd/views.py
+++ nabairqualityd/views.py
@@ -39,7 +39,7 @@ class SettingsView(TemplateView):
         config = Config.load()
         config.next_performance_date = datetime.datetime.now(
             datetime.timezone.utc
-        )
+        ) - datetime.timedelta(seconds=1)
         config.next_performance_type = "today"
         config.save()
         NabAirqualityd.signal_daemon()
